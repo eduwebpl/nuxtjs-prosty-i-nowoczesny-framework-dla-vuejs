9d4ad7485550347a0c93817c3805416170a3a862
Wojciech Połowniak <wojciech.polowniak@gmail.com>
2021-05-05 17:07:45 +0200

    10

---
+++ 10/minipedia/pages/please-login.vue
@@ -0,0 +1,59 @@
+<template>
+  <article class="content">=
+      <Logo />
+      <h2>
+      Please log in
+      </h2>
+  </article>
+</template>
+
+<script>
+export default {
+ computed: {
+  username() {
+   return process.env.NUXT_ENV_USERNAME;
+  }
+ }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
