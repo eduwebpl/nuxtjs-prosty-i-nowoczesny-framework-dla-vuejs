4321c1712abf5189c0beab3cc9add8119dd22316
Wojciech Połowniak <wojciech.polowniak@gmail.com>
2021-05-06 12:40:35 +0200

    16

---
+++ 15/minipedia/layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <article class="content">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </article>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
