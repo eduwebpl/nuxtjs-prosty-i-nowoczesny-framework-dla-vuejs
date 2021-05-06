425ef42deb53de10bf49b274d5e5a801a0935241
Wojciech Połowniak <wojciech.polowniak@gmail.com>
2021-05-06 15:52:58 +0200

    21

---
+++ 21/minipedia/components/ContentSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2 :id="headingId">{{ $props.title }}</h2>
+    <p v-for="paragraph in contentAsAnArray">
+     {{ paragraph }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+ props: {
+   title: {
+    type: String,
+    required: true,
+   },
+   content: {
+    type: String,
+    required: true,
+   }
+ },
+ computed: {
+  contentAsAnArray() {
+   return this.$props.content.split('\n\n');
+  },
+  headingId() {
+   return this.$props.title.replace(' ', '-').toLowerCase();
+  }
+ }
+}
+</script>
+
+<style>
+</style>
